ccb08b6bba0e0d817881386e83a4aac06524ec27
ParkaviMuthaiyan <parkavimuthaiyan@gmail.com>
2023-08-27 23:23:35 +0530

    axios setup

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const apiUrl = "https://technorucswalkinapi.azurewebsites.net";
-
-const api = axios.create({ baseURL: apiUrl });
-
+const apiUrl = "https://technorucswalkinapi.azurewebsites.net/api/";
+const api = axios.create({
+	baseURL: apiUrl,
+	headers: {
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Origin": "*"
+	}
+});
 export default api;
