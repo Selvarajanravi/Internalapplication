960ff78a1cbff4db06043df0d1c52cef19bc7beb
ParkaviMuthaiyan <parkavimuthaiyan@gmail.com>
2023-08-28 15:01:07 +0530

    Added Toast container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { CookiesProvider } from "react-cookie";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Router from "./Router";
 
 function App() {
 	return (
 		<CookiesProvider>
+			<ToastContainer />
 			<Router />
 		</CookiesProvider>
 	);
