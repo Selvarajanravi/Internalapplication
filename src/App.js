216d0e99a052eecbc358081c624fc16d9ea6d6c3
ParkaviMuthaiyan <parkavimuthaiyan@gmail.com>
2023-08-28 13:29:21 +0530

    created the create admin user service

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { CookiesProvider } from "react-cookie";
+import "react-toastify/dist/ReactToastify.css";
 import Router from "./Router";
 
 function App() {
--- src/services/admin.js
+++ src/services/admin.js
@@ -4,3 +4,7 @@ const module = "Admin";
 export async function adminLogin(payload) {
 	return api.post(module + "/AdminLogin", payload);
 }
+
+export async function createAdminUser(payload) {
+	return api.post(module + "/CreateAdminUser", payload);
+}
