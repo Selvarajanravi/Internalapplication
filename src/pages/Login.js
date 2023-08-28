9804572836e0b277b507641d21361f243e0f2dd3
ParkaviMuthaiyan <parkavimuthaiyan@gmail.com>
2023-08-28 23:25:10 +0530

    email and password validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,100 +7,125 @@ import { adminLogin } from "../services/admin";
 
 function Login() {
 	const navigate = useNavigate();
+
 	const [email, setEmail] = useState("");
+	const [emailError, setEmailError] = useState("");
+
 	const [password, setPassword] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+
 	const [validationErrors, setValidationErrors] = useState({});
 	const [isSubmitting, setIsSubmitting] = useState(false);
 	const [cookies, setCookie] = useCookies(["token"]);
 
 	const loginHandler = (e) => {
 		e.preventDefault();
-		setValidationErrors({});
-		setIsSubmitting(true);
-		let payload = {
-			email: email,
-			password: password
-		};
-		adminLogin(payload)
-			.then((r) => {
-				setCookie("token", r.data.token);
-				navigate("/");
-			})
-			.catch((e) => {
-				// setIsSubmitting(false);
-				// if (e.response.data.errors != undefined) {
-				// 	setValidationErrors(e.response.data.errors);
-				// }
-				// if (e.response.data.error != undefined) {
-				// 	setValidationErrors(e.response.data.error);
-				// }
-				toast.warning("you don't have access");
-			})
-			.finally(() => {
-				setIsSubmitting(false);
-			});
+
+		let error = false;
+
+		if (!email.trim()) {
+			setEmailError("Email address is required.");
+			error = true;
+		} else if (!email.trim().match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+			setEmailError("Invalid email format.");
+			error = true;
+		}
+
+		if (!password.trim()) {
+			setPasswordError("Password is required.");
+			error = true;
+		} else if (password.includes(" ")) {
+			setPasswordError("Password should not contain blank spaces.");
+			error = true;
+		}
+
+		if (!error) {
+			setIsSubmitting(true);
+			let payload = {
+				email: email,
+				password: password
+			};
+			adminLogin(payload)
+				.then((r) => {
+					setCookie("token", r.data.token);
+					navigate("/");
+				})
+				.catch((e) => {
+					console.log(e);
+					if (e.response?.data?.status) {
+						toast.warning(e.response.data.status);
+					}
+				})
+				.finally(() => {
+					setIsSubmitting(false);
+				});
+		}
 	};
 
 	return (
-		<>
-			<div className="row justify-content-md-center mt-5">
-				<div className="col-4">
+		<div className="container">
+			<div className="row justify-content-md-center mt-5 ">
+				<div className="col-5">
 					<div className="card">
 						<div className="card-body">
-							<h5 className="card-title mb-4">Sign In</h5>
-							<form onSubmit={loginHandler} autoComplete="off">
-								{Object.keys(validationErrors).length != 0 && (
-									<p className="text-center ">
-										<small className="text-danger">Incorrect Email or Password</small>
-									</p>
-								)}
+							<h5 className="card-title mb-4 text-primary text-center">Sign In</h5>
+							{Object.keys(validationErrors).length != 0 && (
+								<p className="text-center ">
+									<small className="text-danger">Incorrect Email or Password</small>
+								</p>
+							)}
 
-								<div className="mb-3">
-									<label htmlFor="email" className="form-label">
-										Email address
-									</label>
-									<input
-										type="email"
-										className="form-control"
-										id="email"
-										name="email"
-										value={email}
-										required
-										onChange={(e) => {
-											setEmail(e.target.value);
-										}}
-									/>
-								</div>
-								<div className="mb-3">
-									<label htmlFor="password" className="form-label">
-										Password
-									</label>
-									<input
-										type="password"
-										className="form-control"
-										id="password"
-										name="password"
-										value={password}
-										required
-										onChange={(e) => {
-											setPassword(e.target.value);
-										}}
-									/>
-								</div>
-								<div className="d-grid gap-2">
-									<button disabled={isSubmitting} type="submit" className="btn btn-primary btn-block">
-										Login
-									</button>
-									<p className="text-center">
-										Don't have account? <Link to="/register">Register here</Link>
-									</p>
-								</div>
-							</form>
+							<div className="mb-3">
+								<label htmlFor="email" className="form-label">
+									Email
+								</label>
+								<input
+									type="email"
+									className="form-control"
+									id="email"
+									name="email"
+									placeholder="Enter email"
+									value={email}
+									onChange={(e) => {
+										setEmail(e.target.value);
+									}}
+								/>
+								{emailError && <p className="text-danger">{emailError}</p>}
+							</div>
+							<div className="mb-3">
+								<label htmlFor="password" className="form-label">
+									Password
+								</label>
+								<input
+									type="password"
+									className="form-control"
+									id="password"
+									name="password"
+									placeholder="Enter password"
+									value={password}
+									onChange={(e) => {
+										setPassword(e.target.value);
+									}}
+								/>
+								{passwordError && <p className="text-danger">{passwordError}</p>}
+							</div>
+							<div className="d-grid gap-2">
+								<button
+									onClick={loginHandler}
+									disabled={isSubmitting}
+									className="btn btn-primary btn-block"
+								>
+									Login
+								</button>
+								<p className="text-center">
+									Don't have account? <Link to="/register">Register here</Link>
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
